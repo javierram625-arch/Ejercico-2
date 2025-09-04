1e68034bf7611bcd23ddaa6f11cde3bcdda5f75e
javierramirez lopez <javierram625@gmail.com>
2025-09-04 13:58:53 -0600

    agregue la raziz cuadrada

--- clases/avanzadas.py
+++ clases/avanzadas.py
@@ -1,3 +1,5 @@
+import math
+
 class Avanzadas:
     def __init__(self):
         pass
@@ -8,3 +10,10 @@ class Avanzadas:
     def elevarPotencia(self):
         
         pass
+    
+
+def raiz_cuadrada(numero):
+    if numero < 0:
+        raise ValueError("No se puede calcular la raíz cuadrada de un número negativo")
+    return math.sqrt(numero)
+
