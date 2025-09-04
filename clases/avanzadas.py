06befdbf3d2c0f29d27a7ae4008057955553c376
Julio Rangel <juliojeff10@hotmail.com>
2025-09-04 14:02:12 -0600

    Complete el codigo

--- clases/avanzadas.py
+++ clases/avanzadas.py
@@ -1,10 +1,20 @@
 class Avanzadas:
     def __init__(self):
-        pass
+
+        self.num1 = 0
+        self.num2 = 0
+
 
     def leerNumeros(self):
-        pass
+        try:
+            self.num1 = float(input("Ingresa el primer número: "))
+            self.num2 = float(input("Ingresa el segundo número: "))
+        except ValueError:
+            print("Solo se permiten números.")
+            self.leerNumeros()
 
     def elevarPotencia(self):
-        
-        pass
+
+        resultado = self.num1 ** self.num2
+        print(f"{self.num1} elevado a la potencia {self.num2} es: {resultado}")
+        return resultado
